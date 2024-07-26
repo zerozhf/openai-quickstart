ca435ce0ea47e61bae124e0c1c432ada6e3595c7
ZOUZE2 <zero.zou@oocl.com>
2024-07-26 11:06:48 +0800

    feat: completed ai translator day2-2

--- openai-translator/ai_translator/main.py
+++ openai-translator/ai_translator/main.py
@@ -24,4 +24,4 @@ if __name__ == "__main__":
 
     # 实例化 PDFTranslator 类，并调用 translate_pdf() 方法
     translator = PDFTranslator(model)
-    translator.translate_pdf(pdf_file_path, file_format)
+    translator.translate_pdf(pdf_file_path, file_format, args.target_language)
--- openai-translator/ai_translator/utils/argument_parser.py
+++ openai-translator/ai_translator/utils/argument_parser.py
@@ -11,6 +11,7 @@ class ArgumentParser:
         self.parser.add_argument('--openai_api_key', type=str, help='The API key for OpenAIModel. Required if model_type is "OpenAIModel".')
         self.parser.add_argument('--book', type=str, help='PDF file to translate.')
         self.parser.add_argument('--file_format', type=str, help='The file format of translated book. Now supporting PDF and Markdown')
+        self.parser.add_argument('--target_language', type=str, help='the target_language')
 
     def parse_arguments(self):
         args = self.parser.parse_args()
